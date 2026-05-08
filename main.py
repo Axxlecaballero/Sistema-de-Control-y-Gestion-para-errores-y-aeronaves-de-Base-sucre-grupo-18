67962db7103bee70849dbd428b84f1105017f701
Axxlecaballero <Soyjacv@gmail.com>
2026-05-08 19:37:46 -0400

    Cambios

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ def main(page: ft.Page):
 
     # --- DATOS ESTÁTICOS DE PRUEBA (SIMULACIÓN) ---
     aviones = [
-        {"sigla": "YV-800", "horas": 92, "estado": "Crítico"},
-        {"sigla": "YV-520", "horas": 45, "estado": "Operativo"},
-        {"sigla": "YV-115", "horas": 15, "estado": "Operativo"},
-        {"sigla": "YV-940", "horas": 98, "estado": "Crítico"},
+        {"sigla": "DA40-61442", "horas": 92, "estado": "Crítico"},
+        {"sigla": "DA40-61557", "horas": 45, "estado": "Operativo"},
+        {"sigla": "DA42-61651", "horas": 15, "estado": "Operativo"},
+        {"sigla": "DA42-61653", "horas": 98, "estado": "Crítico"},
     ]
 
     fallas_data = [
